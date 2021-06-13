5f449da1fc1117381baf0bae88c5632482342358
ahmfahim <fahimmorshed2019@gmail.com>
2021-06-13 13:12:55 +0600

    recap bug fixed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import react,{useState, useEffect} from 'react'
+import react, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
-  useEffect(() =>{
+  useEffect(() => {
     fetch('http://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  })
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
 
   return (
     <div className="App">
-    {
-      users.map(user=><User name={user.name} email={user.email} phone={user.phone}></User>)
-    }
+      {
+        users.map(user => <User name={user.name} email={user.email} phone={user.phone}></User>)
+      }
     </div>
   );
 }
 
-function User(props){
+function User(props) {
   const userStyle = {
-    boxShadow:"1px 1px 10px gray",
-    background:"#660443ff",
-    color:"white",
-    padding:"20px",
-    width:"500px",
-    margin:"auto",
-    marginTop:"20px"
+    boxShadow: "1px 1px 10px gray",
+    background: "#660443ff",
+    color: "white",
+    padding: "20px",
+    width: "500px",
+    margin: "auto",
+    marginTop: "20px"
   }
-  return(
+  return (
     <div style={userStyle}>
       <h2>Name: {props.name}</h2>
       <p><strong> Email: </strong>{props.email}</p>
