6e385917f1a389b2772e6fea5bfa98e633dacb90
ahmfahim <fahimmorshed2019@gmail.com>
2021-06-13 12:42:25 +0600

    rerap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import react,{useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      users.map(user=><User name={user.name} email={user.email} phone={user.phone}></User>)
+    }
     </div>
   );
+}
+
+function User(props){
+  const userStyle = {
+    boxShadow:"1px 1px 10px gray",
+    background:"#660443ff",
+    color:"white",
+    padding:"20px",
+    width:"500px",
+    margin:"auto",
+    marginTop:"20px"
+  }
+  return(
+    <div style={userStyle}>
+      <h2>Name: {props.name}</h2>
+      <p><strong> Email: </strong>{props.email}</p>
+      <p><strong>Phone:</strong>{props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
